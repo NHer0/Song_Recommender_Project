5acae30e6937237f1580f14a2753e2a2679bb476
NHer0 <91466737+NHer0@users.noreply.github.com>
2021-11-23 17:51:48 +0100

    Update hot_recommender.py

--- hot_recommender.py
+++ hot_recommender.py
@@ -3,13 +3,16 @@ import numpy as np
 
 def hot_recommender(hot_songs):
     
-    user_song = input("Please enter one song: ")
+    user_song = input("Please enter the name of one song you love: ").lower()
     
     if user_song in hot_songs:
         
-        recommended_song = np.random.choice(hot_songs)
+        recommended_song = np.random.choice(hot_songs).title()
+        
+        print(f'We think "{recommended_song}" will like you. Check it out!')
         
     else:
         
         print("The song you provided us is not popular at the moment. Please try again with a hot song.")
         
+        return
