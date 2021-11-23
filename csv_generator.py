b83a0c1722928a4ed4275fa3ef5ba7f4ec3ac9d9
NHer0 <91466737+NHer0@users.noreply.github.com>
2021-11-23 18:20:19 +0100

    Update csv_generator.py

--- csv_generator.py
+++ csv_generator.py
@@ -5,4 +5,4 @@ from billboard100_scrapper import billboard100_scrapper
 # Scrap Billboard 100 and save the data into a csv_file
 
 output = billboard100_scrapper()
-output.to_csv(path_or_buf = "Data/billboard100.csv")
+output.to_csv(path_or_buf="Data/billboard100.csv")
