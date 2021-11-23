a6a424505c15ca8069323b2c6597a652a2a33712
NHer0 <91466737+NHer0@users.noreply.github.com>
2021-11-23 17:51:50 +0100

    Update user_interface.py

--- user_interface.py
+++ user_interface.py
@@ -1,13 +1,14 @@
 # Importing
 
 import pandas as pd
+import numpy as np
 from hot_recommender import hot_recommender
 
 # Loading the songs data
 billboard100 = pd.read_csv(filepath_or_buffer = "Data/billboard100.csv")
-hot_songs = billboard100["song"]
+hot_songs = np.array(billboard100["song"])
 
 
-# # First Prototype, only hot recommender
-# hot_recommender(hot_songs)
+# First Prototype, only hot recommender
+hot_recommender(hot_songs)
 
