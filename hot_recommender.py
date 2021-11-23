0869306a85ebdd5734ce404da8f438c8e871cad7
NHer0 <91466737+NHer0@users.noreply.github.com>
2021-11-23 21:58:03 +0100

    Update hot_recommender.py

--- hot_recommender.py
+++ hot_recommender.py
@@ -1,5 +1,6 @@
 import numpy as np
 import jaro
+import time
 
 
 def hot_recommender_v1(songs_data):
@@ -32,39 +33,59 @@ def hot_recommender_v1(songs_data):
 
 def hot_recommender_v2(songs_data):
 
-    match = False
-
     songs = np.array(songs_data["song"])
     artists = np.array(songs_data["artist"])
-    user_song = input("Please enter the name of one hot song you love: ").lower()
+    match = False
+    on = True
 
-    for song in songs:
+    while on:
 
-        jaro_rating = jaro.jaro_winkler_metric(user_song, song)
+        match = False
+        user_song = input("Please enter the name of one hot song you love: ").lower()
 
-        if jaro_rating > 0.9:
+        for song in songs:
 
-            user_song = song
-            match = True
-            break
+            jaro_rating = jaro.jaro_winkler_metric(user_song, song)
 
-    if match:
+            if jaro_rating > 0.9:
 
-        recommended_song = np.random.choice(songs).title()
-        index = np.where(songs == recommended_song.lower())[0]
+                user_song = song
+                match = True
+                break
 
-        if len(index) == 1:
+        if match:
 
-            recommended_artist = artists[index[0]].title()
+            recommended_song = np.random.choice(songs).title()
+            index = np.where(songs == recommended_song.lower())[0]
+
+            if len(index) == 1:
+
+                recommended_artist = artists[index[0]].title()
+
+            else:
+
+                recommended_artist = artists[np.random.choice(index)].title()
+
+            print(f'We think "{recommended_song}" from "{recommended_artist}" will like you. Check it out!')
 
         else:
 
-            recommended_artist = artists[np.random.choice(index)].title()
+            print("The song you provided us is not popular at the moment. Please try again with a hot song.")
 
-        print(f'We think "{recommended_song}" from "{recommended_artist}" will like you. Check it out!')
+        print("--------------------------")
+        time.sleep(0.5)
+        cont = input("Do you want another recommendation?").lower()
 
-    else:
+        if cont in ["y", "yes"]:
 
-        print("The song you provided us is not popular at the moment. Please try again with a hot song.")
+            print("Awesome! :)")
+            print("--------------------------")
+            pass
+
+        else:
+
+            print("That's a pity :( See you next time!")
+            print("--------------------------")
+            on = False
 
     return
