f4ba2e56ea1aa441ca02441f4aa41f962eda7144
NHer0 <91466737+NHer0@users.noreply.github.com>
2021-11-23 20:52:04 +0100

    Update hot_recommender.py

--- hot_recommender.py
+++ hot_recommender.py
@@ -1,7 +1,8 @@
 import numpy as np
+import jaro
 
 
-def hot_recommender(songs_data):
+def hot_recommender_v1(songs_data):
     
     songs = np.array(songs_data["song"])
     artists = np.array(songs_data["artist"])
@@ -21,9 +22,49 @@ def hot_recommender(songs_data):
             recommended_artist = artists[np.random.choice(index)].title()
         
         print(f'We think "{recommended_song}" from "{recommended_artist}" will like you. Check it out!')
-        
+
     else:
         
         print("The song you provided us is not popular at the moment. Please try again with a hot song.")
         
-        return
+    return
+
+
+def hot_recommender_v2(songs_data):
+
+    match = False
+
+    songs = np.array(songs_data["song"])
+    artists = np.array(songs_data["artist"])
+    user_song = input("Please enter the name of one hot song you love: ").lower()
+
+    for song in songs:
+
+        jaro_rating = jaro.jaro_winkler_metric(user_song, song)
+
+        if jaro_rating > 0.9:
+
+            user_song = song
+            match = True
+            break
+
+    if match:
+
+        recommended_song = np.random.choice(songs).title()
+        index = np.where(songs == recommended_song.lower())[0]
+
+        if len(index) == 1:
+
+            recommended_artist = artists[index[0]].title()
+
+        else:
+
+            recommended_artist = artists[np.random.choice(index)].title()
+
+        print(f'We think "{recommended_song}" from "{recommended_artist}" will like you. Check it out!')
+
+    else:
+
+        print("The song you provided us is not popular at the moment. Please try again with a hot song.")
+
+    return
