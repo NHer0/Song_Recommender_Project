2ec5d7963538a0ac33719c9d496cbac9be691192
NHer0 <91466737+NHer0@users.noreply.github.com>
2021-11-23 20:32:18 +0100

    Update user_interface.py

--- user_interface.py
+++ user_interface.py
@@ -3,9 +3,17 @@
 import pandas as pd
 import numpy as np
 from hot_recommender import hot_recommender
+from billboard100_scrapper import billboard100_scrapper
 
 # Loading the songs data
-billboard100 = pd.read_csv(filepath_or_buffer="Data/billboard100.csv")
+
+try:
+
+    billboard100 = pd.read_csv(filepath_or_buffer="Data/billboard100.csv")
+
+except(FileNotFoundError):
+
+    billboard100 = billboard100_scrapper()
 
 
 # First Prototype, only hot recommender
