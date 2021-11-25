0d402b03cb1a06c7a39922bbb64a2e42ba2bf603
NHer0 <91466737+NHer0@users.noreply.github.com>
2021-11-25 20:49:54 +0100

    Update kmeans_model.py

--- kmeans_model.py
+++ kmeans_model.py
@@ -10,23 +10,26 @@ from tqdm import tqdm
 import pickle
 
 
-def kmeans_model(songs_db, **kwargs):
-
+def kmeans_model(songs_db, n_clusters=6, n_init=20, tol=0):
     # Selecting the features and scaling them
 
-    audiofeatures = song_db.iloc[:, 3:]
+    audiofeatures = songs_db.iloc[:, 3:]
     scaler = StandardScaler()
     scaler.fit(audiofeatures)
     X_scaled = scaler.transform(audiofeatures)
     audiofeatures_scaled = pd.DataFrame(X_scaled, columns=audiofeatures.columns)
 
-
     # Clustering the songs with the audio features
 
-    kmeans = KMeans(n_clusters=)
+    kmeans = KMeans(n_clusters=n_clusters
+                    , random_state=0
+                    , n_init=n_init
+                    , tol=tol
+                    , algorithm="elkan"
+                    , init="k-means++"
+                    )
 
+    kmeans.fit(audiofeatures_scaled)
+    songs_db["cluster"] = kmeans.labels_
 
-
-
-
-
+    return kmeans, scaler, songs_db
