3e5d91a49a0d5dd006b7bd643368bc5e7d6dcd94
NHer0 <91466737+NHer0@users.noreply.github.com>
2021-11-30 12:01:57 +0100

    Update model_generator.py

--- model_generator.py
+++ model_generator.py
@@ -4,14 +4,13 @@ from kmeans_model import kmeans_model
 import pandas as pd
 import pickle
 
-
 # Loading the audio features db
 
 songs_db = pd.read_csv("Data/audiofeatures_db.csv", index_col=0)
 
 # Build the model
 
-kmeans_model, scaler, songs_db = kmeans_model(songs_db, n_clusters=6, n_init=20, tol=0)
+kmeans_model, scaler, songs_db = kmeans_model(songs_db, n_clusters=20, n_init=20, tol=0)
 
 # Save the database with the clusters
 songs_db.to_csv("Data/audiofeatures_clustered_db.csv")
